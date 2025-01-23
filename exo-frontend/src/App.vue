2a16a97b8f153618eb25016329696f27430a5270
Diego Campos <diegocam1246@gmail.com>
2025-01-23 12:58:10 -0500

    feat: Add VueJS and Rocket template

--- exo-frontend/src/App.vue
+++ exo-frontend/src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="grid-container" class="grid-item">
+      <div id="battery-card">
+        <h1>Battery</h1>
+      </div>
+      <div id="telemetry-card" class="grid-item">
+        <h1>Telemetry</h1>
+      </div>
+      <div id="position-card" class="grid-item">
+        <h1>Position</h1>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #313239;
+  margin: 1vh;
+}
+
+#grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 2fr;
+  grid-template-rows: 1fr 2fr;
+  column-gap: 1vh;
+  row-gap: 1vh;
+  height: 98vh;
+}
+
+#battery-card {
+  background-color: white;
+  grid-column: 1 / 4;
+  grid-row: 2 / 3; 
+  border-radius: 10px;
+}
+
+#telemetry-card {
+  background-color: white;
+  grid-column: 2 / 4;
+  grid-row: 1 / 2;
+  border-radius: 10px;
+}
+
+#position-card {
+  background-color: white;
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  border-radius: 10px;
 }
 </style>
