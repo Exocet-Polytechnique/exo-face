c427875cafcd6f22c20d50682f03b2ae004900aa
Diego Campos <diegocam1246@gmail.com>
2025-01-24 23:07:14 -0500

    feat: Add battery grid component

--- exo-frontend/src/App.vue
+++ exo-frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="grid-container" class="grid-item">
       <div id="battery-card">
-        <h1>Battery</h1>
+        <BatteryComponent :data="batteryData" :modules-in-danger="modulesInDanger"/>
       </div>
       <div id="telemetry-card" class="grid-item">
         <h1>Telemetry</h1>
@@ -17,8 +17,26 @@
 
 <script>
 
+import BatteryComponent from './components/Battery.vue';
+
 export default {
   name: 'App',
+  components: {
+    BatteryComponent,
+  },
+  data(){
+    return{
+      batteryData: [
+        { id: 1, status: "Active", duration: "2h", voltage: "3.70", current: "1.23", temperature: "25" },
+        { id: 2, status: "Inactive", duration: "N/A", voltage: "0.00", current: "0.00", temperature: "22" },
+        { id: 3, status: "Active", duration: "2h", voltage: "3.70", current: "1.23", temperature: "25" },
+        { id: 4, status: "Inactive", duration: "N/A", voltage: "0.00", current: "0.00", temperature: "22" },
+        { id: 5, status: "Active", duration: "2h", voltage: "3.70", current: "1.23", temperature: "25" },
+        { id: 6, status: "Inactive", duration: "N/A", voltage: "0.00", current: "0.00", temperature: "22" },
+      ],
+      modulesInDanger: [2, 4, 6]
+    }
+  }
 }
 </script>
 
@@ -57,4 +75,5 @@ body {
   grid-row: 1 / 2;
   border-radius: 10px;
 }
+
 </style>
---
+++ exo-frontend/src/components/Battery.vue
@@ -0,0 +1,89 @@
+<template>
+    <div id="container">
+        <h1 id="title">Batteries</h1>
+        <table id="battery-table" cla>
+            <thead>
+                <tr id="battery-table-header">
+                    <th>Module</th>
+                    <th>Statut</th>
+                    <th>Dur. Est.</th>
+                    <th>Voltage(V)</th>
+                    <th>Courant(A)</th>
+                    <th>Temp.(°C)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in data" :key="row.id" :class="{ danger: modulesInDanger.includes(row.id)}">
+                    <td>{{ row.id }}</td>
+                    <td>{{ row.status }}</td>
+                    <td>{{ row.duration }}</td>
+                    <td>{{ row.voltage }}</td>
+                    <td>{{ row.current }}</td>
+                    <td>{{ row.temperature }}</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BatteryComponent',
+  props: {
+    data: {
+        type: Array,
+        required: true
+    },
+    modulesInDanger:{
+        type: Array,
+        required: false
+    }
+  }
+}
+</script>
+
+<style>
+#container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+#title {
+    margin: 0.5vh;
+    padding: 0;
+    text-align: left;
+    font-size: 5vh;
+    color: #313239;
+}
+
+#battery-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+#battery-table th {
+  padding-top: 0.3vh;
+  padding-left: 0.3vh;
+  padding-bottom: 0.3vh;
+  text-align: left;
+  color: white;
+  font-size: 5vh;
+}
+
+#battery-table-header {
+    background-color: #c12736;
+}
+.danger {
+    background-color: #fe7e44;
+}
+
+#battery-table td {
+  padding-top: 0.3vh;
+  padding-left: 0.3vh;
+  padding-bottom: 0.3vh;
+  text-align: left;
+  font-size: 4vh;
+}
+</style>
